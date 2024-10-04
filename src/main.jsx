8a2f640a3bc355850b20db351e1c9ab05c55d883
dedmaksim03 <sergeevih.08@mail.ru>
2024-10-04 20:15:26 +0300

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
---
+++ src/pages/Error.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Error() {
+  
+    return (
+        <h1>ErrorPage</h1>
+    )
+  }
+  
+  export default Error
